e692a0818479ebba59651a3d0ca955b804915d2b
kholis313354 <kholiskamaluddinw@gmail.com>
2025-03-15 21:00:05 +0700

    Initial commit

---
+++ app/Views/home.php
@@ -0,0 +1,11 @@
+<?= $this->extend('layouts/main') ?>
+
+<?= $this->section('content') ?>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1>Welcome to Nickelfox CI4</h1>
+            <p class="lead">This is a customized template integrated with CodeIgniter 4.</p>
+            <a href="#" class="btn btn-primary">Explore Now</a>
+        </div>
+    </div>
+<?= $this->endSection() ?>
