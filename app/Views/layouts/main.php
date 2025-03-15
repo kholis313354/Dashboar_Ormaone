c288639d97876cf37a6a643877f942b4a3b9ee05
kholis313354 <kholiskamaluddinw@gmail.com>
2025-03-15 21:45:08 +0700

    kedua

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -21,14 +21,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="<?= base_url('lib/owlcarousel/assets/owl.carousel.min.css')?>" rel="stylesheet">
-    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+    <link href="<?= base_url('assets/lib/owlcarousel/assets/owl.carousel.min.css')?>" rel="stylesheet">
+    <link href="<?= base_url('assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css')?>" rel="stylesheet" />
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="<?= base_url('css/bootstrap.min.css')?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/bootstrap.min.css')?>" rel="stylesheet">
     <!-- <?= base_url('assets/css/bootstrap.min.css') ?> -->
     <!-- Template Stylesheet -->
-    <link href="<?= base_url('css/style.css" rel="stylesheet"')?>>
+    <link href="<?= base_url('assets/css/style.css" rel="stylesheet')?>">
 </head>
 
 <body>
@@ -473,16 +473,16 @@
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="<?= base_url('lib/chart/chart.min.js')?>"></script>
-    <script src="<?= base_url('lib/easing/easing.min.js')?>"></script>
-    <script src="<?= base_url('lib/waypoints/waypoints.min.js')?>"></script>
-    <script src="<?= base_url('lib/owlcarousel/owl.carousel.min.js')?>"></script>
-    <script src="<?= base_url('lib/tempusdominus/js/moment.min.js')?>"></script>
-    <script src="<?= base_url('lib/tempusdominus/js/moment-timezone.min.js')?>"></script>
-    <script src="<?= base_url('lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js')?>"></script>
+    <script src="<?= base_url('assets/lib/chart/chart.min.js')?>"></script>
+    <script src="<?= base_url('assets/lib/easing/easing.min.js')?>"></script>
+    <script src="<?= base_url('assets/lib/waypoints/waypoints.min.js')?>"></script>
+    <script src="<?= base_url('assets/lib/owlcarousel/owl.carousel.min.js')?>"></script>
+    <script src="<?= base_url('assets/lib/tempusdominus/js/moment.min.js')?>"></script>
+    <script src="<?= base_url('assets/lib/tempusdominus/js/moment-timezone.min.js')?>"></script>
+    <script src="<?= base_url('assets/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js')?>"></script>
 
     <!-- Template Javascript -->
-    <script src="<?= base_url('js/main.js')?>"></script>
+    <script src="<?= base_url('assets/js/main.js')?>"></script>
 </body>
 
 </html>
